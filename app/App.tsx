eceb84f04f1081d4b10393f7bff52b9a1fff71dd
3M1 <113618121+EmillyNeves@users.noreply.github.com>
2025-05-07 14:37:15 +0000

    rwerwe
    dwda

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,12 @@
 import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/layout/Header";
 import BottomNavigation from "@/components/layout/BottomNavigation";
-
+import { useRouter } from 'next/router';
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="container mx-auto p-4 flex-grow pb-24">
-        {/* Next.js handles routing through the pages directory */}
       </main>
       <BottomNavigation />
       <Toaster />
